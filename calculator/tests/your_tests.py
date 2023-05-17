3b66382164773e48022429ad5dfd9f4f63739351
Ethan Foster <epf@stanford.edu>
2023-05-16 22:52:11 -0700

    Added Makefile and github.txt

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,7 +3,10 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
+# Checks that the program outputs "3" for an input of "6 / 2"
+assert run("6 / 2").output == "3"
+# Checks that the program outputs "24" for an input of "6 * 4"
+assert run("6 * 4").output == "24"
 
 ###
 
